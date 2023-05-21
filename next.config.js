2d2280f3ed48f3fc196fd2e66e9df5faeaad6607
catsonkeyboard <leemeany@gmail.com>
2023-05-21 17:53:56 +0800

    feat:init tauri

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    webpack(config) {
+    config.experiments = { ...config.experiments, topLevelAwait: false }
+    return config
+  },
+}
 
 module.exports = nextConfig
